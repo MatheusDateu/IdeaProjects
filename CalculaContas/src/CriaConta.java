dbb77a0d0617d3fafecfdb159938470f57bc7820
MatheusDateu <mdelmondes5@gmail.com>
2022-04-05 15:59:14 -0400

    Upload
    New classes in CalculaContas project.

--- CalculaContas/src/CriaConta.java
+++ CalculaContas/src/CriaConta.java
@@ -1,6 +1,19 @@
 public class CriaConta {
 
     public static void main(String[] args) {
-        new Conta();
+        Conta firstConta = new Conta();
+        firstConta.saldo = 200;
+        System.out.println(firstConta.saldo);
+
+        firstConta.saldo += 100;
+        System.out.println(firstConta.saldo);
+
+        Conta secondConta = new Conta();
+        secondConta.saldo = 50;
+        System.out.println("Primeira conta: " + firstConta.saldo);
+        System.out.println("Segunda conta: " + secondConta.saldo);
+
+        System.out.println(firstConta);
+        System.out.println(secondConta);
     }
 }
